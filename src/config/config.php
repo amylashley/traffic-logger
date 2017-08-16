1adb1151b1df9f89f348edca8b31a0716d2a9d7b
alashley <alashley@amherst.edu>
2017-08-16 12:28:49 -0400

    :tada: first commit

---
+++ src/config/config.php
@@ -0,0 +1,21 @@
+<?php 
+
+return [
+		
+		/*
+		 |--------------------------------------------------------------------------
+		 | Default Database Connection Name
+		 |--------------------------------------------------------------------------
+		 |
+		 | Here you may specify which of the database connections below you wish
+		 | to use as your default connection.
+		 |
+		 */
+		
+		'db-connection' => env('DB_CONNECTION', 'mysql'),
+		
+		'table-name' => 'log',
+		
+		'trafficlogger' => 'test', //remove after testing
+		
+];
