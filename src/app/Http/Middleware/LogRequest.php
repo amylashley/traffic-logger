8d4b10e28aeb8124dc9ab0debeefb7e66cc86267
alashley <alashley@amherst.edu>
2017-08-17 14:45:51 -0400

    update composer; added Auth info to middleware and re-created it as an after request

--- src/app/Http/Middleware/LogRequest.php
+++ src/app/Http/Middleware/LogRequest.php
@@ -3,26 +3,29 @@
 namespace AmyLashley\TrafficLogger\App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Log;
-use AmyLashley;
+use Illuminate\Support\Facades\Auth;
 use AmyLashley\TrafficLogger\App\Models\Traffic;
 
 class LogRequest
 {
-	
-    public function handle($request, Closure $next)
-    {
-    	$data = ['ipaddress' => $request->getClientIp(),
-    				'username' => '',
-    				'impersonator' => '',
-    				'url' => $request->fullUrl(),
-    				'referer' => $request->headers->get('referer'),
-    				'user_agent' => $request->header('User-Agent'),
-    				'session_id' => ''];
-    	$traffic = new Traffic;
-    	$traffic->fill($data);
-    	$traffic->save();
-    	
-       return $next($request);
-    }
+	public function handle($request, Closure $next)
+	{
+		$response = $next($request);
+		$username = '';
+		if (Auth::user()){
+			$username = Auth::user()->username;
+		}
+		$data = ['ipaddress' => $request->getClientIp(),
+				'username' => $username,
+				'impersonator' => '',
+				'url' => $request->fullUrl(),
+				'referer' => $request->headers->get('referer'),
+				'user_agent' => $request->header('User-Agent'),
+				'session_id' => ''];
+		$traffic = new Traffic;
+		$traffic->fill($data);
+		$traffic->save();
+		
+		return $response;
+	}
 }
